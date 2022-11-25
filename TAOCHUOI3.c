ae4c184868e6b7ae4a418cc4b513c5cade98324d
unknown <longwasu@gmail.com>
2022-11-25 07:25:04 +0700

    7

--- TAOCHUOI3.c
+++ TAOCHUOI3.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-
 int main() {
-	char W[50], S[60];
+	char W[1000], S[1000];
 	int i, j, k = 0;
 	printf("Nhap W = "); gets(W);
-	printf("Cac xau con la:");
+	printf("Cac xau con la:\n");
 	if (W[0] != '\0') {	
 		for (i = 0; i < 3; i++) {
-			j = 0;
 			for (j = 0; j < strlen(W); j++) {
 				S[k] = W[j];
 				k++;
 			}	
 		}
-		int n = strlen(S) / 3;
+		int n = strlen(W);
 		k = 0;
 		for (i = 0; i < n; i++) {
-			printf("\n");
 			for (j = 0; j < 3; j++) {
 				printf("%c",S[k]);
 				k++;			
-			}		
+			}	
+			printf("\n");		
 		}
 	}
-	printf("\n");	
 	return 0;
 }
